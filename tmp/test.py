180cca777f5285f08f2f00c37e60fd9ba2f554a8
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-11-18 10:22:16 +0300

    next

--- tmp/test.py
+++ tmp/test.py
@@ -2,7 +2,7 @@ import skimage
 from skimage import filters
 import matplotlib.pyplot as plt
 
-img = skimage.data.chelsea()
+img = skimage.data.hubble_deep_field()
 img = skimage.color.rgb2gray(img)
 flt = filters.prewitt(img)
 plt.imshow(flt, cmap='gray')
